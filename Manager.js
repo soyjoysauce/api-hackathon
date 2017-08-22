339e1bc32c7c84994a8863cc5b3eb05d61ca755f
JEN-HAO TAN <tan0929@outlook.com>
2017-08-22 13:38:42 -0700

    main.js, and jsdoc for manager.js - James

---
+++ Manager.js
@@ -0,0 +1,39 @@
+/**
+ * Handle all things, because it's manager!
+ * @constructor
+ */
+function Manager(){
+/**
+ * property - hold current select country data
+ */
+
+/**
+ * property - hold the youtube video manager
+ */
+
+/**
+ * property - hold the geoCharts manager
+ */
+
+/**
+ * property - hold the view manager
+ */
+
+
+/**
+ * method - init the manager, fill all property
+ */
+
+/**
+ * method - show video, call function inside view manager
+ */
+
+/**
+ * method - show Chart, call function inside view manager
+ */
+
+/**
+ * method - show data, call function inside view manager
+ */
+
+}
