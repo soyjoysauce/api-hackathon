5a7159e7ddedb8220d952673e5fab2531d635c2c
JEN-HAO TAN <tan0929@outlook.com>
2017-08-22 11:50:05 -0700

    country data ready to use - James

---
+++ Country.js
@@ -0,0 +1,61 @@
+
+/*example
+var c = new Country("china");
+*/
+//if we can got country code from the map, then we can search data with codes instead of search with names
+
+
+function Country(name){
+    this.name = "";
+    this.nativeName = "";
+    this.flag = "";
+    this.capital = "";
+    this.languages = [];
+    this.region = "";
+    this.subregion = "";
+    this.population = "";
+    this.area = "";
+    this.currencies = [];
+
+    this.getData = function(name){
+        var option = {
+            url: "https://restcountries.eu/rest/v2/name/"+name+"?fullText=true",
+            method: "get",
+            dataType: "json",
+            success: function(response){
+                console.log("get country data success => ", response);
+                assignData(response[0]);
+            },
+            error: function(response){
+                console.log("get country data error");
+            }
+        }
+        $.ajax(option);
+    }
+    var self = this;
+    function assignData(countryData){
+        console.log("contryData name", countryData.name);
+        self.name = countryData.name;
+        self.nativeName = countryData.nativeName;
+        self.flag = countryData.flag;
+        self.capital = countryData.capital;
+        for(var i = 0; i < countryData.languages.length; ++i){
+            self.languages.push({
+                name : countryData.languages[i].name, 
+                nativeName : countryData.languages[i].nativeName});
+        };
+        self.region = countryData.region;
+        self.subregion = countryData.subregion;
+        self.population = countryData.population;
+        self.area = countryData.area;
+        for(var i = 0; i < countryData.currencies.length; ++i){
+            self.currencies.push({
+                code: countryData.currencies[i].code,
+                name: countryData.currencies[i].name,
+                symbol: countryData.currencies[i].symbol
+            });
+        }
+    }
+
+    this.getData(name);
+}
